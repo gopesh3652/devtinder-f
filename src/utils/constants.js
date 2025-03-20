9bbe981ee88b642c6ec99c67a16542191b3ef718
gopesh3652 <gopeshkhandelwal10@gmail.com>
2025-03-20 12:27:41 +0530

    Initial commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "/api";
+export const BASE_URL =
+	location.basename === "localhost" ? "http://localhost:3002" : "/api";
 export const GUEST_IMAGE =
 	"https://randomuser.me/api/portraits/thumb/men/69.jpg";

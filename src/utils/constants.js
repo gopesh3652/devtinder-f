98d944806c28e743386bbe336671b9a2e22408cd
gopesh3652 <gopeshkhandelwal10@gmail.com>
2025-03-27 23:35:10 +0530

    Initial commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = "/api";
+export const BASE_URL = "/api";
 // export const BASE_URL = "http://localhost:3002";
-export const BASE_URL =
-	"https://170a-2409-40d4-b-9dfc-3d94-3835-2ed-c816.ngrok-free.app";
-export const GUEST_IMAGE =
-	"https://randomuser.me/api/portraits/thumb/men/69.jpg";
+// export const BASE_URL =
+// "https://170a-2409-40d4-b-9dfc-3d94-3835-2ed-c816.ngrok-free.app";
+// export const GUEST_IMAGE =
+("https://randomuser.me/api/portraits/thumb/men/69.jpg");

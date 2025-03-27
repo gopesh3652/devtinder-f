643e3f71f0e8c44325183b394b2835553702bcb7
gopesh3652 <gopeshkhandelwal10@gmail.com>
2025-03-27 23:34:44 +0530

    Initial commit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,42 +6,42 @@ import { addFeed } from "../utils/feedSlice";
 import UserCard from "./UserCard";
 
 const Feed = () => {
-  const feed = useSelector((store) => store.feed);
-  const dispatch = useDispatch();
+	const feed = useSelector((store) => store.feed);
+	const dispatch = useDispatch();
 
-  const getFeed = async () => {
-    if (feed && feed.length <= 0) return;
+	const getFeed = async () => {
+		if (feed && feed.length <= 0) return;
 
-    try {
-      const res = await axios.get(BASE_URL + "/feed", {
-        withCredentials: true,
-      });
-      dispatch(addFeed(res.data.users));
-    } catch (err) {
-      console.error(err);
-    }
-  };
+		try {
+			const res = await axios.get(BASE_URL + "/feed", {
+				withCredentials: true,
+			});
+			dispatch(addFeed(res.data.users));
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-  useEffect(() => {
-    getFeed();
-  }, []);
+	useEffect(() => {
+		getFeed();
+	}, []);
 
-  if (!feed) return;
+	if (!feed) return;
 
-  if (feed?.length <= 0)
-    return (
-      <div className="flex justify-center m-10">
-        <p>No new user found</p>
-      </div>
-    );
+	if (feed?.length <= 0)
+		return (
+			<div className="flex justify-center m-10">
+				<p>No new user found</p>
+			</div>
+		);
 
-  return (
-    feed && (
-      <div className="flex justify-center my-10">
-        <UserCard user={feed[0]} />
-      </div>
-    )
-  );
+	return (
+		feed && (
+			<div className="flex justify-center my-10">
+				<UserCard user={feed[0]} />
+			</div>
+		)
+	);
 };
 
 export default Feed;

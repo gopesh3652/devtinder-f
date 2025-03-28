630d6227ea823f05131e4fc700c29fa306d91fd6
gopesh3652 <gopeshkhandelwal10@gmail.com>
2025-03-29 01:33:43 +0530

    Initial commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://devtinder-b.onrender.com";
+export const BASE_URL = "https://devtinder-b.onrender.com/api";
 // export const BASE_URL = "http://localhost:3002";
 
 export const GUEST_IMAGE =
